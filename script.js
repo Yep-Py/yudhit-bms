042af6763280fe15e1093decfd04e91e81d92d7e
Yep-Py <5025251136@student.its.ac.id>
2026-08-27 21:25:42 +0700

    feat: finish low-fidelity design

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
 // === Quest 1 ===
 
 // 1. Ambil logo (class="logo")
+const logo = document.querySelector('.logo')
 
 // 2. Klik logo → alert (contoh: alert('Selamat datang!'))
+logo.onclick = () => alert('Selamat datang!')
 
 // === Quest 2 ===
 
 // 3. Ambil semua kartu (.card)
+const cards = document.querySelectorAll('.card')
 
 // 4. Klik kartu → alert isi kartunya (hint: card.querySelector('p').textContent)
+cards.forEach((card) => {
+    card.onclick = () => alert(card.querySelector('p').textContent)
+})
